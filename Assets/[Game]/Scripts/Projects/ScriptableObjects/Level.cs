75d9a47b21c054cff1de8145d7b5daacfb3cc3eb
cemrenazyasar <cemrenazyasar@gmail.com>
2020-12-19 22:53:09 +0300

    obstacle
    obstacle :((

--- Assets/[Game]/Scripts/Projects/ScriptableObjects/Level.cs
+++ Assets/[Game]/Scripts/Projects/ScriptableObjects/Level.cs
@@ -7,6 +7,23 @@ public class Level : ScriptableObject
 {
     [SerializeField]
     List<GameObject> Grounds = new List<GameObject>();
+    [SerializeField]
+    public List<GameObject> Obstacle = new List<GameObject>();
+    [SerializeField]
+    public List<GameObject> Lane= new List<GameObject>();
+    public GameObject GetRandomLevelObject()
+    {
+        try
+        {
+            int rand = Random.Range(0, 6);
+            return Obstacle[rand];
+        }
+        catch (System.Exception ex)
+        {
+            return null;
+        }
+
+    }
 
     public GameObject GetRandomTrack()
     {
