cc19f956c228f2bd0e75649c0c42bd6823f78956
msimalunsal <msimalunsal@gmail.com>
2020-12-27 11:20:32 +0300

    en bastan

--- Assets/[Game]/Scripts/Projects/ScriptableObjects/LevelData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Generic;
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "Endless Runner Level Data", menuName = "Endless Runner/Level Data")]
-
-public class LevelData : ScriptableObject
-{
-    public List<Level> Levels = new List<Level>();
-}
--- README.md
+++
@@ -1,2 +0,0 @@
-# KodluyoruzGameJam
- Kodluyoruz Mobil Oyun Bootcamp için 10 gün boyunca gamejamde geliştirdiğimiz 'Modern Yılbaşı' temalı oyunumuz.
